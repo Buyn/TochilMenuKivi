ae42d31567fb6ab0528e4f54d539922d18ce809f
buyn <buyn.tex@gmail.com>
2020-08-24 20:07:39 +0300

    add screen mainmenu

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.clock import Clock
 from functools import partial
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty, StringProperty
 import time
