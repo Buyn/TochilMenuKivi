ff29866d35801794a0d6dd061d25d9e73622aac2
buyn <buyn.tex@gmail.com>
2020-08-26 03:46:58 +0300

    runing but error on screen tarnsit

--- main.py
+++ main.py
@@ -78,12 +78,15 @@ class Interface(BoxLayout):
 
 # ----------------------------------------------
 # ** def __init__(self, **kwargs): : 
-    # def __init__(self, **kwargs):
-    #     super(Interface, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(Interface, self).__init__(**kwargs)
         # self.label_dateid.text = datetime.now().strftime("%d/%m/%Y")
         # self.label_timeid.text = datetime.now().strftime("%H:%M:%S")
         # self.event_time_updaaate = Clock.schedule_interval(
         #                     self.update_time, 0.5)
+        print ("*******ids-interfase*******")
+        for key, val in self.ids.items():
+            print("key={0}, val={1}".format(key, val))
 
 
 # ----------------------------------------------
@@ -121,6 +124,53 @@ class MainScreen(BoxLayout):
 # ** def __init__(self, **kwargs): : 
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
+        # self.curent_time_label.text = datetime.now().strftime("%d/%m/%Y")
+        # self.curent_date_label.text = datetime.now().strftime("%H:%M:%S")
+        # self.label_dateid.text = datetime.now().strftime("%d/%m/%Y")
+        # self.label_timeid.text = datetime.now().strftime("%H:%M:%S")
+        self.event_time_updaaate = Clock.schedule_interval(
+                            self.update_time, 0.5)
+        print ("*******ids-mainscreen*******")
+        for key, val in self.ids.items():
+            print("key={0}, val={1}".format(key, val))
+
+
+# ----------------------------------------------
+# ** def update_time(self): : 
+    def update_time(self, cb):
+        self.label_dateid.text = datetime.now().strftime("%d/%m/%Y")
+        self.label_timeid.text = datetime.now().strftime("%H:%M:%S")
+
+
+# ----------------------------------------------
+# ** def do_action(self): : 
+    def do_action(self):
+        self.label_dateid.text = datetime.now().strftime("%d/%m/%Y")
+        self.label_timeid.text = datetime.now().strftime("%H:%M:%S")
+        # self.info = "hello world"
+        # print("Time = ", time.time())
+        # print("date = ", datetime.now().strftime("%d/%m/%Y"))
+        # print("Time = ", datetime.now().strftime("%H:%M:%S"))
+
+
+# ----------------------------------------------
+# ** ----------------------------------------------:
+
+
+# * class SharpScreen(BoxLayout): : 
+# ** -SharpScreen------------------------------------:
+class SharpScreen(BoxLayout):
+# class Interface(FloatLayout):
+    # curent_time_label = ObjectProperty()
+    # curent_date_label = ObjectProperty()
+    # label_dateid = ObjectProperty(datetime.now().strftime("%d/%m/%Y"))
+    # label_timeid = ObjectProperty(datetime.now().strftime("%H:%M:%S"))
+    info = StringProperty()
+
+# ----------------------------------------------
+# ** def __init__(self, **kwargs): : 
+    def __init__(self, **kwargs):
+        super(SharpScreen, self).__init__(**kwargs)
         # self.curent_time_label.text = datetime.now().strftime("%d/%m/%Y")
         # self.curent_date_label.text = datetime.now().strftime("%H:%M:%S")
         # self.label_dateid.text = datetime.now().strftime("%d/%m/%Y")
